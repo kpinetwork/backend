3ad2add0d97fa1bdd0ec0ef75e0912e1c8b9cbdd
ridouku <ridouku@gmail.com>
2021-08-26 14:52:29 -0500

    feat: KPI-12 Run commands to generate coverage comment

--- src/tests/service/helloWorld_test.py
+++ src/tests/service/helloWorld_test.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
-
-# import pydash
+import pydash
 
 from src.service.helloWorld import HelloWorld
 
@@ -10,7 +9,6 @@ class TestHelloWorld(TestCase):
         self.hello_world_instance = HelloWorld()
         return
 
-    #
-    # def test_hello_world(self):
-    #     response = self.hello_world_instance.hello_world()
-    #     self.assertTrue(pydash.get(response, "message"))
+    def test_hello_world(self):
+        response = self.hello_world_instance.hello_world()
+        self.assertTrue(pydash.get(response, "message"))
