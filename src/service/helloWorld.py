0674fb4544f927ad2d157a3aa2a0a4fa909e78e8
ridouku <ridouku@gmail.com>
2021-09-20 17:51:38 -0500

    feat: KPI-31 Test code smell

--- src/service/helloWorld.py
+++ src/service/helloWorld.py
@@ -4,3 +4,9 @@ class HelloWorld:
 
     def hello_world(self) -> dict:
         return {"message": "hello this is a test"}
+
+    def foo(self, a):  # NonCompliant
+        b = 12
+        if a == 1:
+            return b
+        return b
