ec6c337ab92d87fa9dfc66e70308fae48bde1f30
Kelly Castro <castrofigueroakelly@gmail.com>
2021-09-23 09:55:32 -0500

    Revert "feat: KPI-17 create db instance configurations (#8)"
    This reverts commit 95b0b7fc9d91c4f0f0bff809d377a75578a83516.

--- infrastructure/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {}
-
-variable "vpc_name" {
-    default = "kpinetworks_vpc"
-}
-
-variable "security_group_name" {
-    default = "kpinetwokrs_rds_group"
-}
--- infrastructure/vpc/vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "kpinetworks_vpc" {
-    cidr_block             = "10.0.0.0/16"
-
-    tags    =  {
-        Name               = var.vpc_name
-    }
-}
