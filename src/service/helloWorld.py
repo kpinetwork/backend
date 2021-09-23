2b1e964644d44278c034571b3c2859bb6b112de4
kellycastrof <castrofigueroakelly@gmail.com>
2021-09-23 12:19:51 -0500

    test: KPI-27 test sonar config

--- src/service/helloWorld.py
+++ src/service/helloWorld.py
@@ -4,3 +4,9 @@ class HelloWorld:
 
     def hello_world(self) -> dict:
         return {"message": "hello this is a test"}
+
+    def test_sonar_cloud(self, a) -> int:
+        b = 12
+        if a == 1:
+            return b
+        return b
