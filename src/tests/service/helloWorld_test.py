05e4c54891ff678d49a1250640bd51683ed4654e
kellycastrof <castrofigueroakelly@gmail.com>
2021-09-23 12:24:24 -0500

    test: KPI-27 remove test sonar cloud

--- src/tests/service/helloWorld_test.py
+++ src/tests/service/helloWorld_test.py
@@ -13,11 +13,3 @@ class TestHelloWorld(TestCase):
     def test_hello_world(self):
         response = self.hello_world_instance.hello_world()
         self.assertTrue(pydash.get(response, "message"))
-
-    def test_test_sonar_cloud_if_branch(self):
-        response = self.hello_world_instance.test_sonar_cloud(1)
-        self.assertIs(12, response)
-
-    def test_test_sonar_cloud(self):
-        response = self.hello_world_instance.test_sonar_cloud(10)
-        self.assertIs(12, response)
