fd70c73e000b61543799a2a982c97a36ccd382a9
Gustavo Arellano <33499443+ridouku@users.noreply.github.com>
2021-10-07 11:55:13 -0500

    feat: KPI-32 implement validations to prevent credentials in code (#19)
    * feat: KPI-32 Test without credentials
    
    * feat: KPI-32 Readme updated

--- infrastructure/sql/rds.tf
+++ infrastructure/sql/rds.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "kpinetworks_db" {
 
   instance_class         = "db.t3.small"
   engine                 = "postgres"
-  engine_version         = "13.2"
+  engine_version         = "13.3"
   port                   = "5432"
 
   db_subnet_group_name   = aws_db_subnet_group.kpinetworks.name
