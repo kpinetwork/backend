db7ae56db8e9fa99319c7898656a02d72731dadb
kellycastrof <castrofigueroakelly@gmail.com>
2021-11-04 10:04:22 -0500

    fix: KPI-54 create policies by environment

--- infrastructure/functions/lambda.tf
+++ infrastructure/functions/lambda.tf
@@ -404,7 +404,6 @@ resource "aws_lambda_function" "get_revenue_sum_by_company_lambda_function" {
 
   layers = [aws_lambda_layer_version.db_lambda_layer.arn]
 
-  layers = [aws_lambda_layer_version.db_lambda_layer.arn]
 
   vpc_config {
     subnet_ids = [var.public_subnet_a_id]
