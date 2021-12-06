9d6f7218c3342952b3e4d89bb8ab96beb8808b03
Daniela Garcia <70675131+dsgarcia8@users.noreply.github.com>
2021-12-06 16:00:32 -0500

    fix: KPI-68 solve rebase conflicts

--- infrastructure/policy/iam.tf
+++ infrastructure/policy/iam.tf
@@ -198,6 +198,26 @@ resource "aws_iam_role" "get_expected_growth_and_margin_lambda_exec_role" {
 EOF
 }
 
+resource "aws_iam_role" "get_revenue_and_ebitda_lambda_exec_role" {
+  name               = "${var.environment}_get_revenue_and_ebitda_lambda_exec_role"
+  path               = "/"
+  description        = "Allows Lambda Function to call AWS services on your behalf."
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_role_policy_attachment" "get_growth_and_margin_lambda_logs" {
   role       = aws_iam_role.get_growth_and_margin_lambda_exec_role.name
   policy_arn = var.aws_iam_policy_logs_arn
@@ -208,6 +228,11 @@ resource "aws_iam_role_policy_attachment" "get_expected_growth_and_margin_lambda
   policy_arn = var.aws_iam_policy_logs_arn
 }
 
+resource "aws_iam_role_policy_attachment" "get_revenue_and_ebitda_lambda_logs" {
+  role       = aws_iam_role.get_revenue_and_ebitda_lambda_exec_role.name
+  policy_arn = var.aws_iam_policy_logs_arn
+}
+
 resource "aws_iam_role_policy_attachment" "get_growth_and_margin_lambda_vpc" {
   role       = aws_iam_role.get_growth_and_margin_lambda_exec_role.name
   policy_arn = var.aws_iam_policy_network_arn
@@ -215,6 +240,11 @@ resource "aws_iam_role_policy_attachment" "get_growth_and_margin_lambda_vpc" {
 
 resource "aws_iam_role_policy_attachment" "get_expected_growth_and_margin_lambda_vpc" {
   role       = aws_iam_role.get_expected_growth_and_margin_lambda_exec_role.name
+  policy_arn = var.aws_iam_policy_network_arn
+}
+
+resource "aws_iam_role_policy_attachment" "get_revenue_and_ebitda_lambda_vpc" {
+  role       = aws_iam_role.get_revenue_and_ebitda_lambda_exec_role.name
   policy_arn = var.aws_iam_policy_network_arn
 }
 
@@ -232,6 +262,14 @@ resource "aws_lambda_permission" "apigw_get_expected_growth_and_margin_lambda" {
   function_name = "${var.environment}_${var.lambdas_names.get_expected_growth_and_margin_lambda_function}"
   principal     = "apigateway.amazonaws.com"
   source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${var.api_gateway_references.apigw_get_expected_growth_and_margin_lambda_function.api_id}/*/${var.api_gateway_references.apigw_get_expected_growth_and_margin_lambda_function.http_method}${var.api_gateway_references.apigw_get_expected_growth_and_margin_lambda_function.resource_path}"
+}
+
+resource "aws_lambda_permission" "apigw_get_revenue_and_ebitda_lambda" {
+  statement_id  = "AllowExecutionFromAPIGatewayRevenueAndEbitda"
+  action        = "lambda:InvokeFunction"
+  function_name = "${var.environment}_${var.lambdas_names.get_revenue_and_ebitda_lambda_function}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${var.api_gateway_references.apigw_get_revenue_and_ebitda_lambda_function.api_id}/*/${var.api_gateway_references.apigw_get_revenue_and_ebitda_lambda_function.http_method}${var.api_gateway_references.apigw_get_revenue_and_ebitda_lambda_function.resource_path}"
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
