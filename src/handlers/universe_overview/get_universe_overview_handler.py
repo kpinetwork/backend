5f52d302fddcabf1c56083d4cef37e3b096a3074
Kelly Castro <castrofigueroakelly@gmail.com>
2021-12-29 09:30:08 -0500

    feat: KPI-00 change filters logic (#62)

--- src/handlers/universe_overview/get_universe_overview_handler.py
+++ src/handlers/universe_overview/get_universe_overview_handler.py
@@ -2,6 +2,7 @@ import json
 import logging
 import datetime
 from universe_overview_service import UniverseOverviewService
+from commons_functions import get_list_param
 from connection import create_db_engine, create_db_session
 from query_builder import QuerySQLBuilder
 from response_sql import ResponseSQL
@@ -17,30 +18,28 @@ overview_service = UniverseOverviewService(session, query_builder, logger, respo
 
 def handler(event, context):
     try:
-        sectors = ""
-        verticals = ""
-        investor_profile = ""
-        growth_profile = ""
-        size = ""
+        sectors = []
+        verticals = []
+        investor_profile = []
+        growth_profile = []
+        size = []
         year = datetime.datetime.today().year
         if event.get("queryStringParameters"):
             params = event.get("queryStringParameters")
-            sectors = params.get("sector", sectors).split(",")
-            verticals = params.get("vertical", verticals).split(",")
-            investor_profile = params.get("investor_profile", investor_profile).split(
-                ","
-            )
-            growth_profile = params.get("growth_profile", growth_profile).split(",")
-            size = params.get("size", size).split(",")
+            sectors = get_list_param(params.get("sector"))
+            verticals = get_list_param(params.get("vertical"))
+            investor_profile = get_list_param(params.get("investor_profile"))
+            growth_profile = get_list_param(params.get("growth_profile"))
+            size = get_list_param(params.get("size"))
             year = params.get("year", year)
 
-        growth_and_margin = overview_service.get_universe_overview(
+        overview = overview_service.get_universe_overview(
             sectors, verticals, investor_profile, growth_profile, size, year
         )
 
         return {
             "statusCode": 200,
-            "body": json.dumps(growth_and_margin, default=str),
+            "body": json.dumps(overview, default=str),
             "headers": {
                 "Content-Type": "application/json",
                 "Access-Control-Allow-Headers": "Content-Type",
