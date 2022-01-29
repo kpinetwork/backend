b862e0b399d84a101c8b5045d0ccd5b37616b51b
Daniela Garcia <70675131+dsgarcia8@users.noreply.github.com>
2022-01-28 19:46:57 -0500

    fix: KPI-103 gix api gateway resource to get users from cognito

--- infrastructure/network/gateways.tf
+++ infrastructure/network/gateways.tf
@@ -160,7 +160,7 @@ resource "aws_api_gateway_resource" "comparison_vs_peers_id" {
 }
 
 resource "aws_api_gateway_resource" "users" {
-  path_part   = "{users}"
+  path_part   = "users"
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
