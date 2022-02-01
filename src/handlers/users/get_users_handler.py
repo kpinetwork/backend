99ceaed7ee457972010e5fad0833b8c5b6db554b
Daniela Garcia <70675131+dsgarcia8@users.noreply.github.com>
2022-02-01 02:12:42 -0500

    feat: KPI-103 add service and test to get users from cognito

--- src/handlers/users/get_users_handler.py
+++ src/handlers/users/get_users_handler.py
@@ -1,38 +1,30 @@
-import json
 import os
+import json
 import boto3
+import logging
+from users_service import UsersService
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+boto3.Session(
+    aws_access_key_id=os.environ.get("ACCESS_KEY"),
+    aws_secret_access_key=os.environ.get("SECRET_KEY"),
+)
+
+cognito = boto3.client("cognito-idp")
+users_service = UsersService(logger, cognito)
 
 
 def handler(event, context):
     try:
-        cognito = boto3.client(
-            "cognito-idp",
-            region_name=os.environ.get("REGION"),
-            aws_access_key_id=os.environ.get("ACCESS_KEY"),
-            aws_secret_access_key=os.environ.get("SECRET_KEY"),
-        )
 
         poolId = os.environ.get("USER_POOL_ID")
-        users = get_users(cognito, poolId)
-        mapped_users = [
-            {"username": user["Username"], "email": user["Attributes"][0]["Value"]}
-            for user in users
-        ]
-
-        for user in mapped_users:
-            groups = cognito.admin_list_groups_for_user(
-                Username=user["username"], UserPoolId=poolId
-            )
-            filter_groups = [
-                group["GroupName"]
-                for group in groups["Groups"]
-                if "Google" not in group["GroupName"]
-            ]
-            user.update({"roles": filter_groups})
+        users = users_service.get_users(poolId)
 
         return {
             "statusCode": 200,
-            "body": json.dumps(mapped_users, default=str),
+            "body": json.dumps(users, default=str),
             "headers": {
                 "Content-Type": "application/json",
                 "Access-Control-Allow-Headers": "Content-Type",
@@ -47,20 +39,3 @@ def handler(event, context):
             "body": json.dumps({"error": str(error)}),
             "headers": {"Content-Type": "application/json"},
         }
-
-
-def get_users(client, pool_id):
-    users = []
-    next_page = None
-    kwargs = {"UserPoolId": pool_id, "AttributesToGet": ["email"]}
-
-    users_remain = True
-    while users_remain:
-        if next_page:
-            kwargs["PaginationToken"] = next_page
-        response = client.list_users(**kwargs)
-        users.extend(response["Users"])
-        next_page = response.get("PaginationToken", None)
-        users_remain = next_page is not None
-
-    return users
