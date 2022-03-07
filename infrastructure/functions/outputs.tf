16a3e0f32649fa9b3c1dbe07bc16dbd79426c030
Daniela Garcia <70675131+dsgarcia8@users.noreply.github.com>
2022-03-07 18:35:20 -0500

    fix: KPI-119 delete unused resources

--- infrastructure/functions/outputs.tf
+++ infrastructure/functions/outputs.tf
@@ -3,19 +3,6 @@ output "lambdas_invoke_arns" {
     "get_company_lambda_function": aws_lambda_function.get_company_lambda_function.invoke_arn
     "get_all_companies_lambda_function": aws_lambda_function.get_all_companies_lambda_function.invoke_arn
     "get_all_public_companies_lambda_function": aws_lambda_function.get_all_public_companies_lambda_function.invoke_arn
-    "get_metrics_lambda_function": aws_lambda_function.get_metrics_function.invoke_arn
-    "get_metric_by_company_id_lambda_function": aws_lambda_function.get_metric_by_company_id_function.invoke_arn
-    "get_average_metrics_lambda_function": aws_lambda_function.get_average_metrics_function.invoke_arn
-    "get_average_metrics_by_cohort_lambda_function": aws_lambda_function.get_average_metrics_by_cohort_function.invoke_arn
-    "get_metrics_by_cohort_id_lambda_function": aws_lambda_function.get_metrics_by_cohort_id_function.invoke_arn
-    "get_scenarios_lambda_function" : aws_lambda_function.get_scenarios_lambda_function.invoke_arn
-    "get_company_scenarios_lambda_function" : aws_lambda_function.get_company_scenarios_lambda_function.invoke_arn
-    "list_scenarios_lambda_function" : aws_lambda_function.list_scenarios_lambda_function.invoke_arn
-    "get_revenue_sum_by_company_lambda_function" : aws_lambda_function.get_revenue_sum_by_company_lambda_function.invoke_arn
-    "get_cohorts_lambda_function": aws_lambda_function.get_cohorts_function.invoke_arn
-    "get_cohort_by_id_lambda_function": aws_lambda_function.get_cohort_by_id_function.invoke_arn
-    "get_cohort_scenarios_lambda_function": aws_lambda_function.get_cohorts_scenarios_function.invoke_arn
-    "get_revenue_sum_by_cohort_lambda_function" : aws_lambda_function.get_revenue_sum_by_cohort_lambda_function.invoke_arn
     "get_universe_overview_lambda_function": aws_lambda_function.get_universe_overview_lambda_function.invoke_arn
     "get_company_report_vs_peers_lambda_function": aws_lambda_function.get_company_report_vs_peers_lambda_function.invoke_arn
     "get_comparison_vs_peers_lambda_function": aws_lambda_function.get_comparison_vs_peers_lambda_function.invoke_arn
