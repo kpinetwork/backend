9d3cdb654a3188850cd3d0b9ea71b2e5dcaaa15e
Kelly Castro <castrofigueroakelly@gmail.com>
2022-03-18 14:41:42 -0500

    feat: KPI-146 revenue with permissions (#113)
    * feat: KPI-146 revenue with permissions
    
    * fix: KPI-146 fix test coverage

--- src/tests/service/comparison_vs_peers/comparison_vs_peers_test.py
+++ src/tests/service/comparison_vs_peers/comparison_vs_peers_test.py
@@ -77,9 +77,10 @@ class TestComparisonvsPeers(TestCase):
 
         self.assertEqual(filters_out, expected_out)
 
-    def test_remove_revenue(self):
-        data = {"size_cohort": "100+", "revenue": 102}
-        expected_out = {"size_cohort": "100+", "revenue": "100+"}
+    def test_remove_revenue_without_permissions(self):
+        self.company_anonymization.companies = ["2"]
+        data = {"id": "1", "size_cohort": "100+", "revenue": 102}
+        expected_out = {"id": "1", "size_cohort": "100+", "revenue": "100+"}
 
         self.comparison_service_instance.remove_revenue([data])
 
@@ -246,14 +247,13 @@ class TestComparisonvsPeers(TestCase):
         self.mock_response_query_sql(self.company)
         self.mock_response_list_query_sql([self.company, self.comparison])
         self.mock_proccess_comparison_results({"1": self.company, "2": self.comparison})
-
         peers = self.comparison.copy()
-        peers["revenue"] = peers["size_cohort"]
         expected_out = {
             "company_comparison_data": self.company,
-            "rank": {"growth": "2 of 2", "revenue": "1 of 2"},
+            "rank": {},
             "peers_comparison_data": [peers],
         }
+
         get_peers_comparison_out = (
             self.comparison_service_instance.get_peers_comparison(
                 "1", [], [], [], [], [], "2020", False, True
@@ -276,7 +276,7 @@ class TestComparisonvsPeers(TestCase):
 
         get_peers_comparison_out = (
             self.comparison_service_instance.get_peers_comparison(
-                "1", [], [], [], [], [], "2020", True, True
+                "1", [], [], [], [], [], "2020", True, False
             )
         )
 
