7c3e67a1c707993bec74a93ed82d043c33ce4052
FreddyJR1995 <freddy.valverde@epn.edu.ec>
2022-05-04 08:48:09 -0500

    feat: KPI-131 break duplicate code and add test

--- src/tests/handlers/users/get_roles_handler_test.py
+++ src/tests/handlers/users/get_roles_handler_test.py
@@ -2,7 +2,7 @@ import json
 from src.tests.data.data_reader import read
 from unittest import TestCase, mock
 from src.handlers.users.get_roles_handler import handler
-import src.handlers.users.get_roles_handler as roles_handler
+import src.handlers.users.get_users_service_instance as user_service_instance
 
 
 class TestGetRolesHandler(TestCase):
@@ -10,8 +10,8 @@ class TestGetRolesHandler(TestCase):
         self.event = read("sample_event_users.json")
 
     @mock.patch("users_service.UsersService.get_roles")
-    @mock.patch("src.handlers.users.get_roles_handler.boto3")
-    @mock.patch.object(roles_handler, "verify_user_access")
+    @mock.patch("src.handlers.users.get_users_service_instance.boto3")
+    @mock.patch.object(user_service_instance, "verify_user_access")
     def test_get_roles_handler_success_should_return_200_response(
         self, mock_verify_access, mock_boto3, mock_get_roles
     ):
@@ -20,6 +20,7 @@ class TestGetRolesHandler(TestCase):
         mock_get_roles.return_value = expected_out
 
         response = handler(self.event, {})
+        print("response", response)
 
         mock_get_roles.assert_called()
         mock_boto3.assert_not_called()
@@ -27,8 +28,8 @@ class TestGetRolesHandler(TestCase):
         self.assertEqual(response.get("body"), json.dumps(expected_out, default=str))
 
     @mock.patch("users_service.UsersService.get_roles")
-    @mock.patch("src.handlers.users.get_roles_handler.boto3")
-    @mock.patch.object(roles_handler, "verify_user_access")
+    @mock.patch.object(user_service_instance, "boto3")
+    @mock.patch.object(user_service_instance, "verify_user_access")
     def test_get_roles_handler_fail_should_return_error_400_response(
         self, mock_verify_access, mock_boto3, mock_get_roles
     ):
@@ -44,12 +45,12 @@ class TestGetRolesHandler(TestCase):
         self.assertEqual(response.get("body"), json.dumps({"error": error_message}))
 
     @mock.patch("users_service.UsersService.get_roles")
-    @mock.patch("src.handlers.users.get_roles_handler.boto3")
-    @mock.patch.object(roles_handler, "verify_user_access")
+    @mock.patch.object(user_service_instance, "boto3")
+    @mock.patch.object(user_service_instance, "verify_user_access")
     def test_get_users_handler_fail_no_access_400_response(
         self, mock_verify_access, mock_boto3, mock_get_roles
     ):
-        error_message = "No permissions to get roles"
+        error_message = "No permissions to get data"
         mock_verify_access.return_value = False
 
         response = handler(self.event, {})
